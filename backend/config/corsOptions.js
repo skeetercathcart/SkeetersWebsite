553ce3170a995205f6bf2eeb889eb590b11da97e
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-04-17 22:27:29 -0500

    putting in !origin again as a cors option

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -3,8 +3,8 @@ const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
     origin: (origin, callback) => {
 
-        console.log('Incoming CORS request from:', origin)
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        console.log('🔍 Incoming CORS request from:', origin)
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
