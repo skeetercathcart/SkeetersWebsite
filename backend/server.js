787bae72de2c9c912242559c9650d4698b258a69
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-04-17 22:15:20 -0500

    saved that change

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,6 @@ app.all('*', (req, res) => {
     }
 })
 
-app.use(errorHandler)
-
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
