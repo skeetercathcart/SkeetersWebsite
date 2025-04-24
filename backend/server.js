4d73625af81e2d275ea7f45a9652c29209bc4d7d
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-04-24 13:27:28 -0500

    Attempt to fix server side route issue 'client' -> 'frontend'

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,14 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'public', 'client', 'build')))
+app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
 
 app.use('/', require('./routes/root'))
 app.use('/api', require('./routes/api/osrsData'))
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'public', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.use((req, res, next) => {
