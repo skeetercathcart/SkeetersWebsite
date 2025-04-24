6c9974ed3934f64fc7009f31bd341ecb64d591ee
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-04-24 12:59:05 -0500

    server should serve index.html to account for react routing when page refreshes or url is linked to a certain page

--- backend/server.js
+++ backend/server.js
@@ -18,22 +18,15 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use('/', require('./routes/root'))
 app.use('/api', require('./routes/api/osrsData'))
 
 
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.use((req, res, next) => {
     const origin = req.headers.origin;
