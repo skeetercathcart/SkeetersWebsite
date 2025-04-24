b9291581a8aefa61579db10027d4ab53cf3a52d8
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-04-24 14:49:56 -0500

    k

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,45 @@
-require('dotenv').config()
-require('express-async-errors')
-const express = require('express')
-const app = express()
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const connectDB = require('./config/dbConn')
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 3500
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3500;
 
-connectDB()
+// Connect to DB
+connectDB();
 
-app.use(cors(corsOptions))
+// Middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-
-app.use(cookieParser())
-
+// Request logger
 app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.path}`);
-    next();
-  });
-
-app.use('/', express.static(path.join(__dirname, '..', 'frontend', 'build')))
-
-
-app.use('/api', require('./routes/api/osrsData'))
-app.use('/', require('./routes/root'))
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+  console.log(`Incoming request: ${req.method} ${req.path}`);
+  next();
 });
 
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    console.log('Request Origin:', origin);
-    next();
-  });
+app.use('/', express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
+app.use('/api', require('./routes/api/osrsData'));
+
+app.use('/', require('./routes/root'));
+
+app.get('*', (req, res) => {
+  console.log(`Serving React for route: ${req.originalUrl}`);
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
 
 mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-})
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
 
 mongoose.connection.on('error', err => {
-    console.log(err)
-})
+  console.log(err);
+});
