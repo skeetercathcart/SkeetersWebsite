5590acf94b6c052b8748b0b26442b5b21e7db702
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-05-05 18:39:02 -0500

    fix route differences that I messed up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import useTitle from './hooks/useTitle';
 import { Routes, Route } from 'react-router-dom';
 import GlobalLayout from './components/GlobalLayout';
 import LandingPage from './components/LandingPage';
-import RunescapeCalc from './components/RunescapeCalc/RunescapeCalc';
-import DevTools from './components/DevTools/DevTools'
+import RunescapeCalc from './components/RunescapeCalc';
 import NotFound from './components/NotFound'
 import BlackJack from './components/BlackJack/BlackJack';
 
@@ -15,7 +14,6 @@ function App() {
       <Route path = "/" element={<GlobalLayout/>}>
         <Route index element={<LandingPage/>}></Route>
         <Route path="runescapecalc" element={<RunescapeCalc/>}/>
-        <Route path="devtools" element = {<DevTools/>}/>
         <Route path="blackjack" element = {<BlackJack/>}/>
         <Route path = '*' element = {<NotFound/>}/>
       </Route>
