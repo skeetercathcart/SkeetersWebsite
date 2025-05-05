972b66091496d6b89499907442fd9971c3052019
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-05-05 18:53:51 -0500

    changes to landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -5,13 +5,16 @@ const LandingPage = () => {
     return (
         <div className = "full-container">
             <div className = "about-text-container">
-                <div className = "name-text">
-                    Skeeter Cathcart
-                </div>
-                <div className = "about-text">
-                    <p>Passionate developer, life-long learner from the Twin Cities</p>
-                    <p>Graduate from Minnesota State University, Mankato</p>
-                </div>
+                <ul className = "intro-text">
+                    <li className = "name-text"><strong>Skeeter Cathcart</strong></li>
+                    <li>Passionate developer, life-long learner from the Twin Cities.</li>
+                    <li>Computer Science graduate from Minnesota State University, Mankato.</li>
+                    <br></br>
+                    <li>Welcome to my website portfolio!</li>
+                    <li>I created this using a MERN stack and hosted with Render.</li>
+                    <br></br>
+                    <li>Note: The backend spins up on-demand, so thing like Runescape Calc may take a moment to load.</li>
+                </ul>
                 <div className = "socials-container">
                     <a href = "https://github.com/skeetercathcart">
                         <img  className = "socials-link" style ={{border: '2px black solid', borderRadius: '100%'}} src = "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" height = "44px" width = "44px" alt = "Github Logo"></img>
