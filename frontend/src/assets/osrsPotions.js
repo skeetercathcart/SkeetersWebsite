8bb7918f44153d727aa1867ca2d125bbfcab6f88
Skeeter Cathcart <skeetercathcart@gmail.com>
2025-07-16 13:00:19 -0500

    Adding potion assets

---
+++ frontend/src/assets/osrsPotions.js
@@ -0,0 +1,61 @@
+const potions = [
+  {
+      id: 'rangingPotion',
+      label: 'Ranging Potion',
+      stat: 'ranged',
+      xboost: 1.10,
+      fboost: 4,
+      icon: 'https://oldschool.runescape.wiki/images/Ranging_potion%284%29.png?095ed',
+    },
+  {
+      id: 'superStrengthPotion',
+      label: 'Super Strength Potion',
+      stat: 'strength',
+      xboost: 1.15,
+      fboost: 5,
+      icon: 'https://oldschool.runescape.wiki/images/Super_strength%284%29.png?fa231',
+    },
+    {
+      id: 'superAttackPotion',
+      label: 'Super Attack Potion',
+      stat: 'attack',
+      xboost: 1.15,
+      fboost: 5,
+      icon: 'https://oldschool.runescape.wiki/images/Super_attack%284%29.png?ff13c',
+    },
+    {
+      id: 'superDefensePotion',
+      label: 'Super Defense Potion',
+      stat: 'defense',
+      xboost: 1.15,
+      fboost: 5,
+      icon: 'https://oldschool.runescape.wiki/images/Super_defence%284%29.png?ff13c',
+    },
+    {
+      id: 'attackPotion',
+      label: 'Attack Potion',
+      stat: 'attack',
+      xboost: 1.10,
+      fboost: 3,
+      icon: 'https://oldschool.runescape.wiki/images/Attack_potion%284%29.png?758a9',
+    },
+    {
+      id: 'strengthPotion',
+      label: 'Strength Potion',
+      stat: 'strength',
+      xboost: 1.10,
+      fboost: 3,
+      icon: 'https://oldschool.runescape.wiki/images/Strength_potion%284%29.png?a8970',
+    },
+    {
+      id: 'defensePotion',
+      label: 'Defense Potion',
+      stat: 'defense',
+      xboost: 1.10,
+      fboost: 3,
+      icon: 'https://oldschool.runescape.wiki/images/Defence_potion%284%29.png?6b57c',
+    },
+    
+  ];
+
+  export { potions }
